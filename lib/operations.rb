5c2ad739c4821b66dde9303da1594ebd6eefa267
TSephanya Brun <tsbrun@icloud.com>
2020-07-15 00:03:19 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
 def unsafe?(speed)
 	if speed > 60 || speed < 40
-		return true
+		true
 	else
-		return false
+		false
 	end
 end
 
 
 
 def not_safe?(speed)
-  speed > 60 || speed < 40 ? true : false
+  speed < 40 || speed > 60 ? true : false
 end
